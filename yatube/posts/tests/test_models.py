925b8ab0ee54b33c5f81b50568719af63cc96c68
Timofey Kondakov <tkondakov98@yandex.ru>
2022-08-11 21:21:02 +0400

    Не сразу понял про обьект группы, но вроде бы так?

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from ..models import Group, Post
 from posts import constants
+from ..models import Group, Post
 
 User = get_user_model()
 
